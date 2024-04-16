db1e41c4df8570658d8d7b343fe704109f9d7640
ahmedjilani123 <ahmedjilani0011@gmail.com>
2024-04-16 11:31:02 +0530

    remove cors

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const cors = require('cors');
+//const cors = require('cors');
 require("dotenv").config();
 const dbConnection = require("./src/api/db/Database.connect");
-//const UserRouter = require('./src/api/routes/User.router');
-const { allRegister } = require('./src/api/controllers/user.controller');
+const UserRouter = require('./src/api/routes/User.router');
 const app = express();
 // app.use(cors({
 //     origin: process.env.CORS_ORIGIN,
@@ -12,18 +11,16 @@ const app = express();
 const port = process.env.PORT || 3000
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
-// app.use("/api",UserRouter)
-app.get('/api/users/register',allRegister)
+app.use("/api",UserRouter)
 dbConnection().then(()=>{
-     
-    
+    app.listen(port,function(){
+        console.log("listening on port "+ port);
+    });  
 }).catch((err)=>{
     console.log("Listening Failed : "+err.message);
 })                  
 
-app.listen(port,function(){
-    console.log("listening on port "+ port);
-});
+
 
 
 
