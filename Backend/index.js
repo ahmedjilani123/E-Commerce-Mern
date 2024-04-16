8461eff18006b46938b65203a67a33741236e75a
ahmedjilani123 <ahmedjilani0011@gmail.com>
2024-04-16 11:07:19 +0530

    add

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 3000
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
-app.use('/api/v1/users',UserRouter)
+app.use(UserRouter)
 dbConnection().then(()=>{
         app.listen(port,function(){
             console.log("listening on port "+ port);
